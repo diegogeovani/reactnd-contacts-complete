060ab142e5f7d467e718acc33b2037405d66bea8
Diego Santos <diego.santos@olxbr.com>
2018-03-20 10:19:55 -0300

    Refactoring: destructure ListContacts component variables

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -19,19 +19,22 @@ class ListContacts extends Component {
     }
 
     render() {
+        const { contacts, onDeleteContact } = this.props
+        const { query } = this.state;
+
         let showingContacts
-        if (this.state.query) {
-            const match = new RegExp(escapeRegExp(this.state.query), 'i')
-            showingContacts = this.props.contacts.filter((c) => match.test(c.name))
+        if (query) {
+            const match = new RegExp(escapeRegExp(query), 'i')
+            showingContacts = contacts.filter((c) => match.test(c.name))
         } else {
-            showingContacts = this.props.contacts;
+            showingContacts = contacts;
         }
         showingContacts.sort(sortBy('name'))
 
         return (
             <div className="list-contacts">
                 <div className="list-contacts-top">
-                    <input className="search-contacts" type="text" placeholder="Search contacts" value={this.state.query}
+                    <input className="search-contacts" type="text" placeholder="Search contacts" value={query}
                         onChange={(event) => this.updateQuery(event.target.value)} />
                 </div>
                 <ol className='contact-list'>
@@ -45,7 +48,7 @@ class ListContacts extends Component {
                                 <p>{contact.email}</p>
                             </div>
                             <button className='contact-remove'
-                                onClick={() => this.props.onDeleteContact(contact)}>Remove</button>
+                                onClick={() => onDeleteContact(contact)}>Remove</button>
                         </li>
                     ))}
                 </ol>
